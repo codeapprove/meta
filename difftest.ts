42345eef10927a5d5aac84ee810cd67796991edc
Sam Stern <hatboysam@gmail.com>
2021-01-21 22:21:07 +0000

    Test sub-line diffs

--- difftest.ts
+++ difftest.ts
@@ -2,9 +2,9 @@
 
 // Here I'm going to change the leading whitespace before the args
 myFunction.doWithArgs(
-  arg1,
-  arg2.getProperty()
+    arg1,
+    arg2.getProperty()
 );
 
 // Here I'm going to add a third import to see if it handles the commas right
-import { foo, bar } from "library";
+import { foo, bar, baz } from "library";
