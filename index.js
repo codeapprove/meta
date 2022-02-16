2e178d9b4c5857f2ddcb1825fc2a28f62fa9556d
Sam Stern <hatboysam@gmail.com>
2022-02-16 10:24:43 -0500

    Add more to index.js

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ function main(args) {
   console.info(args);
 }
 
+function fooBar(x, y) {
+  const z = x + y;
+  return z;
+}
+
+function barBaz(q, z) {
+  const a = q + z;
+  return a;
+}
+
 main();
